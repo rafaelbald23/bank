91e5330dbe415c0ffb4bf1654a59e75b90ed8eaa
Rafael Theobald <141092579+rafaelbald23@users.noreply.github.com>
2024-04-01 23:16:25 -0300

    Sistema em PY
    Abra o codigo e leia!

---
+++ banco.py
@@ -0,0 +1,68 @@
+menu = """ 
+Bem-vindo ao Sistema de Caixa:
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUE = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informar o valor do depósito: "))
+        print("Depósito realizado com sucesso!")
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        print("Saque realizado com sucesso!")
+
+        excedeu_saldo = valor > saldo
+        
+        excedeu_limite = valor > limite
+
+        excedeu_saque = numero_saques >= LIMITE_SAQUE
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! Você não tem limite suficiente.")    
+
+        elif excedeu_saque:
+            print("Operação falhou! Você não tem saque suficiente.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================== EXTRATO ==================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("===============================================")
+
+    elif opcao == "q":
+        print("Obrigado por ser nosso cliente, volte sempre!")
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
